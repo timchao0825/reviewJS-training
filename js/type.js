3cb38cf9499a6412a2a984f3058f20fcd76f7640
Tim <tim.chao0825@gmail.com>
2021-11-08 21:42:00 +0800

    type of, value of

--- js/type.js
+++ js/type.js
@@ -1,12 +1,33 @@
-console.log('type js init')
+// ----
+// basic type
+// ----
+var str = 'string'
+// console.log(str)
+// console.log(typeof str)
 
-var empty
-console.log(empty)
-console.log(typeof empty)
-var testNull = null
-console.log(testNull)
-console.log(typeof testNull) // object
+// str.val = 100
+// console.log(str.val)
 
-// test
-console.log(undefined == null)
-console.log(undefined === null)
+// var objStr = new String('obj string')
+// console.log('obj.str ==>', typeof objStr)
+// console.log(objStr + 3)
+// objStr.val = 100
+// console.log('obj.str value ==>', objStr.valueOf())
+
+// ----
+// undefined null
+// ----
+// var empty
+// console.log(empty)
+// console.log(typeof empty)
+// var testNull = null
+// console.log(testNull)
+// console.log(typeof testNull) // object
+
+// // test
+// console.log(undefined == null)
+// console.log(undefined === null)
+
+// ----
+// truthy falsy
+// ----
