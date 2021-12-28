e0143e9cb6b263e71f8123685e4e2adef01bb512
Tim <tim.chao0825@gmail.com>
2021-12-28 19:12:33 +0800

    object Oriented review

--- js/objectOriented.js
+++ js/objectOriented.js
@@ -1,1 +1,9 @@
 console.log('object oriented js init')
+
+function Foo() {}
+
+var foo = new Foo()
+
+console.log(Foo.prototype)
+console.log(foo.__proto__)
+console.log(Foo.prototype === foo.__proto__)
